ec98c3786ba2da4d562cc25d3f97999092797386
07kundan <kundankumar28884@gmail.com>
2024-02-21 13:24:14 +0530

    started learning contextAPI

---
+++ 11_ContextAPI_projectOne/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-red-500 text-amber-400">aur batao</div>
+    </>
+  )
+}
+
+export default App
