2d1d60be988987cacc1e1b14fb220202b2f157dc
07kundan <kundankumar28884@gmail.com>
2024-02-22 00:19:59 +0530

    commit x

--- test.js
+++ test.js
@@ -1,23 +1,14 @@
-
-
-let a=1;
-a=!a
-
-console.log(a ? "true" : "false")
-let b='x'
-// if(a){
-    // console.log('true')
-// }
-// else{
-    // console.log('false')
-// }
-if(b){
-    console.log('true')
+let y
+if(y){
+    console.log(true)
 }
-else{
-    console.log('false')
-}
+else console.log(false)
+// undefined gives flase
 
+function hello() {
+    y=5
+}
+console.log(y)
 
 
 // let arr=[3,4,5,6,7,8,9]
